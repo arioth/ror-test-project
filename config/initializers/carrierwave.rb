4d53e03949d9878fe1b569cc95f3090076712e53
Angel Vargas <arioth17@gmail.com>
2016-02-07 15:00:19 -0430

    Add AWS S3 to carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,12 @@
 CarrierWave.configure do |config|
-  config.storage = :file
+  config.storage = :fog                                               # required
+  config.fog_credentials = {
+    provider:              'AWS',                                     # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                  # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],              # required
+  }
+  config.fog_directory  = 'ror-project-development'                   # required
+  config.fog_public     = true                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=315576000" }  # optional, defaults to {}
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
